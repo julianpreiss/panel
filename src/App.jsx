7ab91a7ff3d6987cdcd3275af3cc75d7d43df0e4
Julian Preiss <julianpreiss23@gmail.com>
2022-03-06 22:32:43 -0300

    [App] agrega redirecciones en login

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Body from './pages/Body';
 import Login from './pages/Login';
-import { useAuth } from './context/Auth.Context';
+import Bookings from './components/Bookings/BookingsView';
 
 function AuthDiv(props) {
-  const { state } = useAuth();
-  return state.isAuthenticated ? props.children : null;
+  return props.isAuth ? props.children : null;
 }
 
 function AuthRoute(props) {
-  const { state } = useAuth();
-  return state.isAuthenticated ? props.children : <Navigate to="/" />;
+  return props.isAuth ? props.children : <Navigate to="/" />;
 }
 
 function App() {
-  const { state: AuthState, dispatch: AuthDispatch } = useAuth();
   const [isAuth, setAuth] = useState(false);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if(AuthState.isAuthenticated) {
-      navigate('/panel/reservas');
-    } else {
-      navigate('/');
-    }
-  } , [AuthState]);
 
   return (
     <div className="App">
@@ -33,8 +22,8 @@ function App() {
         <Body />
       </AuthDiv> 
       <Routes>
-        <Route path="/" element={<Login onLogin={()=> {setAuth(true); navigate('/panel')} }  />} />
-        <Route path="/panel" exact element={<AuthRoute isAuth={isAuth}><Body /></AuthRoute>} />
+        <Route path="/" element={<Login onLogin={()=> {setAuth(true); navigate("/reservas")}} />} />
+        <Route path="/panel" exact element={<AuthRoute isAuth={isAuth}><Body /> </AuthRoute> }/>
       </Routes>
     </div>
   );
